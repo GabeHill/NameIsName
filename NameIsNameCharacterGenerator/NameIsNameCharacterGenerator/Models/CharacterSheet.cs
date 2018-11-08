7d713e2b9aac12ca44df47f9d94f8eaed7097c0b
RyanKivett <36863659+RyanKivett@users.noreply.github.com>
2018-11-07 18:32:10 -0700

    Added Ability scores

--- NameIsNameCharacterGenerator/NameIsNameCharacterGenerator/Models/CharacterSheet.cs
+++ NameIsNameCharacterGenerator/NameIsNameCharacterGenerator/Models/CharacterSheet.cs
@@ -27,10 +27,39 @@ namespace NameIsNameCharacterGenerator.Models {
 
         public Background background { get; set; }
 
+        public int Str { get; set; }
+        public int Dex { get; set; }
+        public int Con { get; set; }
+        public int Int { get; set; }
+        public int Wis { get; set; }
+        public int Cha { get; set; }
+
         public CharacterSheet(Race r, CharacterClass c, Background b) {
             race = r;
             characterClass = c;
             background = b;
+
+            Str = SetAbilityScore();
+            Dex = SetAbilityScore();
+            Con = SetAbilityScore();
+            Int = SetAbilityScore();
+            Wis = SetAbilityScore();
+            Cha = SetAbilityScore();
+        }
+
+        private int SetAbilityScore()
+        {
+            int total = 0;
+            total += RollD6();
+            total += RollD6();
+            total += RollD6();
+            return total;
+        }
+
+        private int RollD6()
+        {
+            Random r = new Random();
+            return r.Next(1, 7);
         }
     }
 }
