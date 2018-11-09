a2f3efe4b81a6f2530789ea461372b165a3c3548
Richard Sanchez <superpoptart1@gmail.com>
2018-11-08 17:25:04 -0700

    Added the CSVs to the project

--- NameIsNameCharacterGenerator/NameIsNameCharacterGenerator/Controllers/HomeController.cs
+++ NameIsNameCharacterGenerator/NameIsNameCharacterGenerator/Controllers/HomeController.cs
@@ -4,22 +4,32 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace NameIsNameCharacterGenerator.Controllers {
-    public class HomeController : Controller {
-        public ActionResult Index() {
+namespace NameIsNameCharacterGenerator.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
             return View();
         }
 
-        public ActionResult NewCharacter() {
+        public ActionResult NewCharacter()
+        {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
-        public ActionResult AllCharacters() {
+        public ActionResult AllCharacters()
+        {
             ViewBag.Message = "Your contact page.";
 
             return View();
         }
+
+        //public ActionResult CreateNewCharacter()
+        //{
+        //    // TODO: adding a random characer to the database
+        //}
     }
 }
