9c87f16e54b3a86b0578158cb2d716b9613d14eb
Richard Sanchez <superpoptart1@gmail.com>
2018-11-12 17:29:38 -0700

    Fixed the param issue with the character sheet

--- NameIsNameCharacterGenerator/NameIsNameCharacterGenerator/Models/CharacterSheet.cs
+++ NameIsNameCharacterGenerator/NameIsNameCharacterGenerator/Models/CharacterSheet.cs
@@ -45,8 +45,20 @@ namespace NameIsNameCharacterGenerator.Models
         public string Ideal { get; set; }
         public string PersonalityTrait { get; set; }
 
-        public CharacterSheet(Race r, CharacterClass c, Background b)
+        public CharacterSheet()
         {
+            Array values = Enum.GetValues(typeof(Race));
+            Random random = new Random();
+            Race r = (Race)values.GetValue(random.Next(values.Length));
+
+            Array values2 = Enum.GetValues(typeof(CharacterClass));
+            Random random2 = new Random();
+            CharacterClass c = (CharacterClass)values2.GetValue(random2.Next(values2.Length));
+
+            Array values3 = Enum.GetValues(typeof(Background));
+            Random random3 = new Random();
+            Background b = (Background)values3.GetValue(random3.Next(values3.Length));
+
             SetBackground(b);
 
             race = r;
@@ -64,10 +76,10 @@ namespace NameIsNameCharacterGenerator.Models
         private void SetBackground(Background b)
         {
             //You need to change to file paths based on your personal computers
-            Bond = SetBackgroundTrait(ReadFile(@"C:\Users\Ryan Neumont Laptop\source\repos\GabeHill\NameIsName\NameIsNameCharacterGenerator\NameIsNameCharacterGenerator\CSVs\Bond.csv", b), 6);
-            Flaw = SetBackgroundTrait(ReadFile(@"C:\Users\Ryan Neumont Laptop\source\repos\GabeHill\NameIsName\NameIsNameCharacterGenerator\NameIsNameCharacterGenerator\CSVs\Flaws.csv", b), 6);
-            Ideal = SetBackgroundTrait(ReadFile(@"C:\Users\Ryan Neumont Laptop\source\repos\GabeHill\NameIsName\NameIsNameCharacterGenerator\NameIsNameCharacterGenerator\CSVs\Ideals.csv", b), 6);
-            PersonalityTrait = SetBackgroundTrait(ReadFile(@"C:\Users\Ryan Neumont Laptop\source\repos\GabeHill\NameIsName\NameIsNameCharacterGenerator\NameIsNameCharacterGenerator\CSVs\PersonalityTrait.csv", b), 8);
+            Bond = SetBackgroundTrait(ReadFile(@"\CSVs\Bond.csv", b), 6);
+            Flaw = SetBackgroundTrait(ReadFile(@"\CSVs\Flaws.csv", b), 6);
+            Ideal = SetBackgroundTrait(ReadFile(@"\CSVs\Ideals.csv", b), 6);
+            PersonalityTrait = SetBackgroundTrait(ReadFile(@"\CSVs\PersonalityTrait.csv", b), 8);
         }
 
         private List<string> ReadFile(string filename, Background b)
